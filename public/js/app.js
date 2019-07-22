c13e3add42c54dd36b99bf7c371c415cd9b23f16
paigesmith3794 <paigesmith3794@gmail.com>
2019-07-22 07:28:15 -0700

    app.js in progress

---
+++ public/js/app.js
@@ -0,0 +1,132 @@
+$(document).ready(function () {
+
+    var articleList = [];
+    var articleId = '';
+    var article = '';
+    var previousArticle = 0;
+    var currentArticle = 0;
+    var nextArticle = 0;
+
+    $('#comments').addClass('hidden');
+
+
+    $.getJSON('/scrape', function () {
+
+    });
+
+
+    $(document).on('click', '#getArticles', function () {
+
+        $.getJSON('/articles', function (data) {
+            articleList = data;
+            article = articleList[0];
+            showArticle(article);
+        });
+
+    });
+
+
+    $(document).on('click', '.previous', function () {
+
+        article = articleList[previousArticle];
+        currentArticle = previousArticle;
+        showArticle(article);
+
+    });
+
+
+    $(document).on('click', '.next', function () {
+
+        article = articleList[nextArticle];
+        currentArticle = nextArticle;
+        showArticle(article);
+
+    });
+
+
+    $(document).on('click', '#addComment', function () {
+
+        if ($('#commentText').val() != '') {
+            var name = $('#name').val();
+            var comment = $('#commentText').val();
+            $.post("/addcomment/" + articleId, { name: name, comment: comment }, function (e) {
+                e.preventDefault();
+
+            });
+
+            $('#name').val('');
+            $('#commentText').val('');
+            showComments(articleId);
+
+        }
+
+    });
+
+
+    $(document).on('click', '.deletecomment', function () {
+        commentId = this.id;
+
+        $.ajax({
+
+            method: "GET",
+            url: "/deletecomment/" + commentId
+        }).done(function (data) {
+
+        })
+
+        showComments(articleId);
+
+    });
+
+
+    var showArticle = function (article) {
+
+        $('#title').text(article.title);
+        $("#image").removeClass("hidden");
+        $('#image').attr('src', article.imgLink);
+        $('#summary').text(article.summary);
+        $("#readArticle").removeClass("hidden");
+        $('#article').attr('href', article.storyLink);
+        $("#getArticles").addClass("hidden");
+        $("#navigation").empty();
+
+        previousArticle = currentArticle - 1;
+
+        if (previousArticle >= 0) {
+            $('#navigation').append('<button id="' + previousArticle + '" class="btn btn-primary previous">Previous Article</button>');
+        } else {
+            $('#navigation').append('<button id="' + previousArticle + '" class="btn btn-primary disabled previous">Previous Article</button>');
+
+        }
+
+        nextArticle = currentArticle + 1;
+
+        if (nextArticle < articleList.length) {
+            $('#navigation').append('<button id="' + nextArticle + '" class="btn btn-primary pull-right next">Next Article</button>');
+        } else {
+            $('#navigation').append('<button id="' + nextArticle + '" class="btn btn-primary pull-right disabled next">Next Article</button>');
+
+        }
+
+        articleId = article._id;
+
+        showComments(articleId);
+
+    }
+
+
+    var showComments = function (articleId) {
+
+        $("#comments").removeClass("hidden");
+        $("#articleComments").empty();
+        var commentText = '';
+        $.getJSON('comments/' + articleId, function (data) {
+            for (var i = 0; i < data.length; i++) {
+                commentText = commentText + '<div class="well"><span id="' + data[i]._id + '" class="glyphicon glyphicon-remove text-danger deletecomment"></span> ' + data[i].comment + ' - ' + data[i].name + '</div>';
+            }
+
+            $("#articleComments").append(commentText);
+
+        });
+    }
+});
