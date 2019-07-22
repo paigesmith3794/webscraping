c55865915e7a4fc9350ece710185aaccdb002fff
paigesmith3794 <paigesmith3794@gmail.com>
2019-07-22 08:03:56 -0700

    edited connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,5 @@
 var mongoose = require("mongoose");
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-mongoose.connect(MONGODB_URI);
-
 mongoose.connect("mongodb://localhost/allthenewsdb", function (error) {
     if (error) throw error;
     console.log("Database connected");
